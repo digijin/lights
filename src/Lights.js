ef9fe3fd09b0d17e43ac7910067507b0fc2a52a5
digijin <james@digijin.com>
2016-11-10 14:06:14 +1100

    initial tests passing

--- src/Lights.js
+++ src/Lights.js
@@ -3,8 +3,19 @@ const GREEN = 'green'
 const YELLOW = 'yellow'
 const RED = 'red'
 
+const TENMIN = 10*60*1000;
+const FIVEMIN = 5*60*1000;
+const THIRTYSEC = 30*1000;
+
 export default class Lights{
   getStatus(time = new Date()) {
+    time = time.getTime(); //make numeric
+    let green = (time % FIVEMIN > FIVEMIN - THIRTYSEC) ? YELLOW:GREEN;
+    if(time%TENMIN > FIVEMIN){
+      return {ew:RED, ns:green}
+    }else{
+      return {ns:RED, ew:green}
+    }
 
   }
   getChanges(start, end) {
--- test/Lights.spec.js
+++ test/Lights.spec.js
@@ -23,36 +23,36 @@ describe('Lights', () => {
       it('should be red for the first 5 minutes', () => {
         let time = new Date();
         time.setMinutes(2);
-        expect(lights.getStatus(time).ns).to.be('red')
+        expect(lights.getStatus(time).ns).to.equal('red')
       })
       it('should be green between 5 and 9 minutes', () => {
         let time = new Date()
         time.setMinutes(7);
-        expect(lights.getStatus(time).ns).to.be('green')
+        expect(lights.getStatus(time).ns).to.equal('green')
       });
       it('should be yellow from 9:30 to 10', () => {
         let time = new Date()
         time.setMinutes(9);
         time.setSeconds(45);
-        expect(lights.getStatus(time).ns).to.be('yellow')
+        expect(lights.getStatus(time).ns).to.equal('yellow')
       })
     })
     describe('EW', () => {
       it('should be green for the first 4.5 minutes', () => {
         let time = new Date();
         time.setMinutes(2);
-        expect(lights.getStatus(time).ew).to.be('green')
+        expect(lights.getStatus(time).ew).to.equal('green')
       })
       it('should be yellow between 4.5 and 5 minutes', () => {
         let time = new Date()
         time.setMinutes(4);
         time.setSeconds(45);
-        expect(lights.getStatus(time).ew).to.be('yellow')
+        expect(lights.getStatus(time).ew).to.equal('yellow')
       });
       it('should be red from 5 to 10 mins', () => {
         let time = new Date()
         time.setMinutes(7);
-        expect(lights.getStatus(time).ew).to.be('red')
+        expect(lights.getStatus(time).ew).to.equal('red')
       })
     })
   });
